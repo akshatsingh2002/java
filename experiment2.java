93f81172924ba6fe31b049a41ff579070a2bab8f
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-03-03 18:50:26 +0530

    commit

---
+++ experiment2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+ class experiment2 {
+    public static void main(String[] Akshat){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter your name");
+        String str=input.nextLine();
+        System.out.println("MY name is "+str);
+
+    }
+}
